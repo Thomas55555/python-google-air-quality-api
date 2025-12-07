782ef4d71a15f16ea09c09540b0f0095ad6464af
Thomas55555 <59625598+Thomas55555@users.noreply.github.com>
2025-12-07 23:09:08 +0100

    Remove unused functions (#129)
    * Remove unused functions
    
    * remove more

--- src/google_air_quality_api/api.py
+++ src/google_air_quality_api/api.py
@@ -1,27 +1,13 @@
 """API for Google Air Quality bound to Home Assistant OAuth."""
 
 import logging
-from math import cos, floor, log, pi, radians, tan
-
-from aiohttp import ClientResponse
 
 from .auth import Auth
-from .const import API_BASE_URL
 from .model import AirQualityData
-from .model_reverse_geocoding import PlacesResponse
 
 _LOGGER = logging.getLogger(__name__)
 
 CURRENT_CONDITIONS = "currentConditions:lookup"
-
-
-def latlon_to_tile(lat: float, lon: float, zoom: int) -> tuple[int, int]:
-    """Convert lat/lon to tile coordinates."""
-    lat_rad = radians(lat)
-    n = 2.0**zoom
-    x_tile = floor((lon + 180.0) / 360.0 * n)
-    y_tile = floor((1.0 - log(tan(lat_rad) + 1.0 / cos(lat_rad)) / pi) / 2.0 * n)
-    return x_tile, y_tile
 
 
 class GoogleAirQualityApi:
@@ -46,18 +32,3 @@ class GoogleAirQualityApi:
         return await self._auth.post_json(
             CURRENT_CONDITIONS, json=payload, data_cls=AirQualityData
         )
-
-    async def async_heatmap(self, lat: float, long: float, zoom: int) -> ClientResponse:
-        """Get all air quality data."""
-        x, y = latlon_to_tile(lat, long, zoom)
-        heat_map_uri = (
-            f"{API_BASE_URL}/mapTypes/UAQI_RED_GREEN/heatmapTiles/{zoom}/{x}/{y}"
-        )
-        return await self._auth.get(heat_map_uri)
-
-    async def async_reverse_geocode(
-        self, lat: float, long: float, granularity: str = "GEOMETRIC_CENTER"
-    ) -> PlacesResponse:
-        """Get a location from coordinates."""
-        geocode_uri = f"https://geocode.googleapis.com/v4beta/geocode/location/{lat},{long}?granularity={granularity}"
-        return await self._auth.get_json(geocode_uri, data_cls=PlacesResponse)
